b6932b2475c4857bdad5488548a56326988ba472
maks <maxdabrowski93@o2.pl>
2021-02-02 21:59:50 +0100

    second version-7

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-/*app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
-});*/
+app.get('/*', (req,res) => {
+ // res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.send('łakamakafą')
+});
 
 app.listen(process.env.PORT || 8080);
 console.log('Console listening!');

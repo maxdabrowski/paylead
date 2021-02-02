7bfb657a1c133a0799c0b6500404590be6eda7b9
maks <maxdabrowski93@o2.pl>
2021-02-02 22:22:05 +0100

    second version-8

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/client2'));
 
 app.get('/*', (req,res) => {
- // res.sendFile(path.join(__dirname + 'dist/index.html'));
-  res.send('łakamakafą')
+ res.sendFile(path.join(__dirname + 'dist/client2/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);

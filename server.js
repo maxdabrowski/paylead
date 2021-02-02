eb4949315e09d7b2afa7b7e1c0755d7ec39e0f42
maks <maxdabrowski93@o2.pl>
2021-02-02 20:54:35 +0100

    second version

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(proces.env.PORT || 8080);
-
-app.get('/*', function(req,res){
+app.get('/*', (req,res) => {
   res.sendFile(path.join(__dirname + 'dist/index.html'))
 });
 
+app.listen(proces.env.PORT || 8080);
 console.log('Console listening!');

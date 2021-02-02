acfb3bbada51fd61b143c39762b3687d85aa27dd
maks <maxdabrowski93@o2.pl>
2021-02-02 21:12:56 +0100

    second version-2

--- serverw.js
+++ serverw.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (req,res) => {
-  res.sendFile('index.html', {root:'./dist/'})
+  res.sendFile(path.join(__dirname + 'dist/index.html'))
 });
 
 app.listen(proces.env.PORT || 8080);
